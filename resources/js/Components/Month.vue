9bb1d3e842ad893880b3f69aa437277174b950b8
Robert Cooper <rob.cooper@sellonlinedirect.com>
2022-12-03 18:48:44 +0000

    wip

--- resources/js/Components/Month.vue
+++ resources/js/Components/Month.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
-    <h1 class="text-sm mb-8">Month - {{ month }}</h1>
+    <h1 class="text-sm mb-8 font-bold">Month - {{ props.month+1 }}</h1>
+    <week v-for="week in weeksInMonth" :key="week.start" :week="week"></week>
   </div>
 </template>
 
 <script setup>
 
-defineProps({
+import Week from "@/Components/Week";
+import { DateTime, Interval } from "luxon";
+
+const props = defineProps({
   month: {
     type: Number,
     required: true,
   },
 });
+
+// create an array of this months days
+const startOfMonth = DateTime.local().set({ day: 1, month: props.month + 1, year: 2022 });
+const endOfMonth = DateTime.fromObject({day:1, month:props.month+1, year: 2022}).plus({months:1});//.
+const weeksInMonth = Interval.fromDateTimes(
+  startOfMonth, endOfMonth
+).splitBy({ weeks: 1 });
+
 
 </script>
---
+++ resources/js/Components/Week.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mb-4">
+    <h1 class="text-sm mb-8">Week</h1>
+    <div v-for="day in daysInWeek" :key="day">
+      {{ day.start.weekday}} - {{ day.start.toString() }}
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+// import Day from "@/Components/Day";
+
+const props = defineProps({
+  week: {
+    type: Object,
+    required: true,
+  },
+});
+
+// get the days for this week
+const daysInWeek = props.week.splitBy({ days: 1 });
+
+console.log(daysInWeek);
+
+</script>
