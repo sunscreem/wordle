16dbfb80799285cb7851366ea8a55c5047c41e3f
Robert Cooper <rob.cooper@sellonlinedirect.com>
2022-12-03 19:17:02 +0000

    wip

--- resources/js/Components/Month.vue
+++ resources/js/Components/Month.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="text-sm mb-8 font-bold">Month - {{ props.month+1 }}</h1>
-    <week v-for="week in weeksInMonth" :key="week.start" :week="week"></week>
+    <!-- <week v-for="week in weeksInMonth" :key="week.start" :week="week"></week> -->
   </div>
 </template>
 
@@ -19,10 +19,58 @@ const props = defineProps({
 
 // create an array of this months days
 const startOfMonth = DateTime.local().set({ day: 1, month: props.month + 1, year: 2022 });
-const endOfMonth = DateTime.fromObject({day:1, month:props.month+1, year: 2022}).plus({months:1});//.
-const weeksInMonth = Interval.fromDateTimes(
+const endOfMonth = DateTime.fromObject({day:1, month:props.month+1, year: 2022}).plus({months:1}).minus({days:1});
+
+const daysInMonth = Interval.fromDateTimes(
   startOfMonth, endOfMonth
-).splitBy({ weeks: 1 });
+);
+
+// console.log(daysInMonth.start);
+
+const startOfMonthPaddingNeeded = daysInMonth.start.weekday-1;
+const endOfMonthPaddingNeeded = 7-daysInMonth.end.weekday;
+
+// construct the weeks
+const allDays = [...Array(startOfMonthPaddingNeeded).keys()].map((i) => {
+  return { day:null }
+})
+.concat(daysInMonth.splitBy({ days: 1 }).map((day) => {
+  // rob you are here. Check the console log.
+  // I think you don't need to be returning the days now. I think you maybe just replace this with the worlde score for the day.
+  return { day:day.start } ;
+}))
+.concat([...Array(endOfMonthPaddingNeeded).keys()].map((i) => {
+  return { day:null }
+}));
+
+// maybe you don't even need to faff with weeks if this is in a 7x grid?
+console.log(allDays);
+
+
+
+  // return {
+  //   start: daysInMonth.start.minus({days: startOfMonthPaddingNeeded-i}),
+  //   end: daysInMonth.start.minus({days: startOfMonthPaddingNeeded-i}),
+  // }
+// }).concat(daysInMonth.splitBy({days:1})).concat([...Array(endOfMonthPaddingNeeded).keys()].map((i) => {
+//   return {
+//     start: daysInMonth.end.plus({days: i+1}),
+//     end: daysInMonth.end.plus({days: i+1}),
+//   }
+// }));
+
+console.log(daysInMonth.end,daysInMonth.end.weekday,endOfMonthPaddingNeeded);
+// const days = daysInMonth.map((day) => {
+//   return day.start;
+// });
+
+// console.log(days);
+// rob you are here. days In Month is an interval.
+// maybe find out what day of the week the first day is
+// and pad it out with empty days
+// then then same for the end of the month
+// then split that into weeks.
+
 
 
 </script>
